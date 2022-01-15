af6365e7365cc472b003cf32ca928253b73794bf
shinp09 <57585657+shinp09@users.noreply.github.com>
2022-01-15 18:32:29 +0900

    feat: todo controller

--- controller/todo_controller.go
+++ controller/todo_controller.go
@@ -1,2 +1,11 @@
 //リクエストを元にモデルの各処理を呼び出しレスポンスを返却する
 package controller
+
+import "net/http"
+
+type TodoController interface {
+	GetTodos(w http.ResponseWriter, r *http.Request)
+	PostTodo(w http.ResponseWriter, r *http.Request)
+	PutTodo(w http.ResponseWriter, r *http.Request)
+	DeleteTodo(w http.ResponseWriter, r *http.Request)
+}
